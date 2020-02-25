dbe1b73ec9f489ab11b1d6469d831adf565c119d
thiagohbfialho <thiago.fialhoti@gmail.com>
2020-02-25 01:39:30 -0300

    aprendendo flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,43 @@
+import './widgets/user_transaction.dart';
+
+import './models/transaction.dart';
 import 'package:flutter/material.dart';
+
 
 void main() => runApp(FastOrder());
 
 class FastOrder extends StatelessWidget {
-  void answerQuestion(){
-    print('Answer chosen');
-  }
+  final List<Transaction> transactions = [
+    
+  ];
+  //String titleInput;
+  //String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?'
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Fast Order"),
         ),
-        body: Column(
-          children: [
-            Text(
-              questions.elementAt(0)
-            ),
-            RaisedButton(
-              child: Text('Answer 1'), 
-              onPressed: answerQuestion
-            ),
-            RaisedButton(
-              child: Text('Answer 2'), 
-              onPressed: answerQuestion
-            ),
-            RaisedButton(
-              child: Text('Answer 3'), 
-              onPressed: answerQuestion
-            ),
-            RaisedButton(
-              child: Text('Answer 4'), 
-              onPressed: answerQuestion
-            ),
-            RaisedButton(
-              child: Text('Answer 5'), 
-              onPressed: answerQuestion
-            ),
-          ],
+        body: SingleChildScrollView(
+                  child: Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  color: Colors.blue,
+                  child: Text('CHART!'),
+                  elevation: 5,
+                ),
+              ),
+              UserTransaction(),
+            ],
+          ),
         ),
       ),
     );
